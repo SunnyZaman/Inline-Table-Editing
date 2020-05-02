b2352a0a26ff9a1d21fd57b37d6acf08bdfb473f
Zaman <Sunny.Zaman@ontario.ca>
2020-05-02 19:46:39 -0400

    created service fees component

---
+++ src/app/components/service-fees/service-fees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-fees',
+  templateUrl: './service-fees.component.html',
+  styleUrls: ['./service-fees.component.scss']
+})
+export class ServiceFeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
