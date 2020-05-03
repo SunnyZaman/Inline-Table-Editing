b017a55ac897c2368065ce5bde6ee7886ebc687c
Zaman <Sunny.Zaman@ontario.ca>
2020-05-02 22:50:17 -0400

    setup service fee

--- src/app/components/service-fees/service-fees.component.ts
+++ src/app/components/service-fees/service-fees.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceFee } from 'src/app/models/service-fee';
 
 @Component({
   selector: 'app-service-fees',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-fees.component.scss']
 })
 export class ServiceFeesComponent implements OnInit {
-
+  tableData:ServiceFee[];
+  tableHeaders;
   constructor() { }
 
   ngOnInit(): void {
   }
+  edit(fee:ServiceFee){
 
+  }
+  select(fee:ServiceFee){
+    
+  }
 }
