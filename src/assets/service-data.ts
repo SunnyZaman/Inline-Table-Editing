5aa6d1c2ce0f79f3b619a72712fe7dd29d3d5258
Zaman <Sunny.Zaman@ontario.ca>
2020-05-03 14:17:48 -0400

    updated service fee model

--- src/assets/service-data.ts
+++ src/assets/service-data.ts
@@ -3,21 +3,49 @@ export default {
         {
             Id: 0,
             ServiceType: "Application",
-            Cost: 0,
+            Cost: "$0/hour",
             Quantity: 0,
             Total: 0
         },
         {
             Id: 1,
-            ServiceType: "Reproduction",
-            Cost: 0,
+            ServiceType: "Audting",
+            Cost: "$0/hour",
             Quantity: 0,
             Total: 0
         },
         {
             Id: 2,
+            ServiceType: "Consultation",
+            Cost: "$30/hour",
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 3,
+            ServiceType: "Delivery",
+            Cost: "$0/hour",
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 4,
+            ServiceType: "Inspection",
+            Cost: "$7.50/15 min",
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 5,
+            ServiceType: "Reproduction",
+            Cost: "$0/hour",
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 6,
             ServiceType: "Shipping",
-            Cost: 0,
+            Cost: "$0/hour",
             Quantity: 0,
             Total: 0
         }
