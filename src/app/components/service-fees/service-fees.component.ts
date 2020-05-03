829722fda147265d64f81d4fb94d6ff568d9ca55
Zaman <Sunny.Zaman@ontario.ca>
2020-05-02 23:10:33 -0400

    added data to table

--- src/app/components/service-fees/service-fees.component.ts
+++ src/app/components/service-fees/service-fees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceFee } from 'src/app/models/service-fee';
+import * as serviceData from "src/assets/service-data";
 
 @Component({
   selector: 'app-service-fees',
@@ -8,8 +9,31 @@ import { ServiceFee } from 'src/app/models/service-fee';
 })
 export class ServiceFeesComponent implements OnInit {
   tableData:ServiceFee[];
-  tableHeaders;
-  constructor() { }
+  tableHeaders = [
+    {
+      key: "ServiceType",
+      text: "Service",
+      cell: (row: any) => `${row.ServiceType}`
+    },
+    {
+      key: "Cost",
+      text: "Cost",
+      cell: (row: any) => `${row.Cost}`
+    },
+    {
+      key: "Quantity",
+      text: "Quantity",
+      cell: (row: any) => `${row.Quantity}`
+    },
+    {
+      key: "Total",
+      text: "Total",
+      cell: (row: any) => `${row.Total}`
+    }
+  ];
+  constructor() {
+    this.tableData = serviceData.default.Data;
+   }
 
   ngOnInit(): void {
   }
---
+++ src/assets/service-data.ts
@@ -0,0 +1,25 @@
+export default {
+    Data: [
+        {
+            Id: 0,
+            ServiceType: "Application",
+            Cost: 0,
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 1,
+            ServiceType: "Reproduction",
+            Cost: 0,
+            Quantity: 0,
+            Total: 0
+        },
+        {
+            Id: 2,
+            ServiceType: "Shipping",
+            Cost: 0,
+            Quantity: 0,
+            Total: 0
+        }
+    ]
+}
