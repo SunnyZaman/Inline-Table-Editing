addd99f05b52bd0dd97ee5e00232cc593f9ef01d
Zaman <Sunny.Zaman@ontario.ca>
2020-05-04 17:27:13 -0400

    added inline style in cell row

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
-
+  comments = [];
+  tableHeaders = [
+    {
+      key: "Information",
+      text: "Information",
+      style:{'color': '#355796', 'font-weight': 700},
+      cell: (row: any) => `${row.Information}<br/><span class="comment-info">${row.Date}</span>`
+    },
+    {
+      key: "Comment",
+      text: "Comment",
+      cell: (row: any) => `${row.Comment}`
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    this.comments = [
+      {
+        Information: "Sunny Zaman",
+        Date: new Date().toDateString(),
+        Comment: "This application is built with Angular Material"
+      }
+    ]
   }
 
 }
